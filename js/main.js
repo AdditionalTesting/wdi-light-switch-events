f2edba9483ba941a70671f5850cafb00f7a04551
AlB <albert.burc@gmail.com>
2017-08-17 16:02:53 -0400

    fully functioning javascript lightswitch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
 
+  var clickThis = document.querySelector('button.switch.on');
+  var pageBody = document.querySelector('body');
+  var text = document.querySelector('h1.status');
 
+  clickThis.addEventListener('click', function(){
+    if (clickThis.className === 'switch on'){
+      clickThis.className = 'switch off';
+      pageBody.className = 'dark';
+      text.innerText = 'Who done did this?';
+    } else if (clickThis.className === 'switch off') {
+      clickThis.className = 'switch on';
+      pageBody.className = 'light'
+      text.innerText = 'Why so bright???';
+    }
+  })
+
+})
